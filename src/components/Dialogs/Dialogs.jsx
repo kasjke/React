c3cb69d38e5c87e3dcbea92718912fc86d9ca867
kasjke <kasmatuy2k17@gmail.com>
2023-03-21 17:16:00 +0200

    initial commit

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import w from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+  return (
+    <div className={w.dialog + " " + w.active}>
+      <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={w.dialog}>{props.message}</div>;
+};
+
+const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "Dima" },
+    { id: 2, name: "Misha" },
+    { id: 3, name: "Sasha" },
+    { id: 4, name: "Masha" },
+    { id: 5, name: "Tanya" },
+  ];
+
+  return (
+    <div className={w.dialogs}>
+      <div className={w.dialogsitems}>
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+      </div>
+      <div className={w.messages}>
+        <Message message="Hello" />
+        <Message message="How are you" />
+        <Message message="Are you ok?" />
+        <Message message="yes,good!" />
+      </div>
+    </div>
+  );
+};
+
+export default Dialogs;
